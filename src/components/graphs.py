9cbb679a7f66171e568cd13c4c635f35dad0c3d4
Fblade <lucas.toirkens@gmail.com>
2025-05-24 14:13:41 +0200

    Optimize redrawing to only draw relevant items

--- src/components/graphs.py
+++ src/components/graphs.py
@@ -21,6 +21,10 @@ class TimeSeriesGraphWidget(QWidget):
         self.layout.addWidget(self.canvas)
         self.setLayout(self.layout)
 
+        self._values = None
+        self._previous_values = None
+
+        self._ax = None
         self._bars = None
         self._animation = None
 
@@ -45,45 +49,61 @@ class TimeSeriesGraphWidget(QWidget):
 
     def reset_values(self):
         """Resets certain values when changing data source."""
+        self._ax = None
         self._bars = None
         self._animation = None
+        self._values = None
+        self._previous_values = None
         self.figure.clear()
 
     def load_data(self, df: polars.DataFrame, title: str):
-        # self.figure.clear()
-        ax = self.figure.add_subplot(111, facecolor=self.colors["background"])
-
         # Convert Polars timestamps to matplotlib dates
         timestamps = df["timestamp"].to_list()
         values = df["studied_minutes"].to_list()
 
+        if self._ax is None:
+            self._ax = self.figure.add_subplot(111, facecolor=self.colors["background"])
+
+            # Labels
+            self._ax.set_title(title, color=self.colors["text"])
+            self._ax.set_xlabel("Timestamp", color=self.colors["text"])
+            self._ax.set_ylabel("Studied Minutes", color=self.colors["text"])
+
+            # Scope
+            max_value = max(values) if values else 1
+            self._ax.set_ylim(0, max_value * 1.1)
+
+            # Ticks
+            self._ax.tick_params(axis="x", colors=self.colors["text"])
+            self._ax.tick_params(axis="y", colors=self.colors["text"])
+
+            # Grid color
+            self._ax.grid(True, axis="y", color=self.colors["grid"])
+
+            # Use custom formatter for x-labels
+            set_xaxis_labels(self._ax, timestamps)
+
+        if self._previous_values is None:
+            self._previous_values = [0] * len(values)
+        else:
+            self._previous_values = self._values
+            # Match size
+            if len(self._previous_values) < len(self._values):
+                self._previous_values = self._previous_values + [0] * (
+                    len(self._values) - len(self._previous_values)
+                )
+
         self._timestamps = timestamps
         self._values = values
 
-        self._bars = ax.bar(
+        self._bars = self._ax.bar(
             timestamps,
-            [0] * len(values),
+            self._previous_values,
             color=self.colors["bar"],
             edgecolor=self.colors["bar_edge"],
             linewidth=0.5,
             width=self.get_bar_width(timestamps),
         )
-
-        ax.set_title(title, color=self.colors["text"])
-        ax.set_xlabel("Timestamp", color=self.colors["text"])
-        ax.set_ylabel("Studied Minutes", color=self.colors["text"])
-
-        max_value = max(values) if values else 1
-        ax.set_ylim(0, max_value * 1.1)
-
-        ax.tick_params(axis="x", colors=self.colors["text"])
-        ax.tick_params(axis="y", colors=self.colors["text"])
-
-        # Use custom formatter for x-labels
-        set_xaxis_labels(ax, timestamps)
-
-        # Grid color
-        ax.grid(True, axis="y", color=self.colors["grid"])
 
         # Animate
         self._animation = FuncAnimation(
@@ -106,6 +126,9 @@ class TimeSeriesGraphWidget(QWidget):
         frames = 30
         frame = i / frames
         eased_t = self.ease_in_out_quad(frame)
-        for bar, value in zip(self._bars, self._values, strict=False):
-            bar.set_height(eased_t * value)
+        for bar, value, previous_value in zip(
+            self._bars, self._values, self._previous_values, strict=False
+        ):
+            delta = value - previous_value
+            bar.set_height(eased_t * delta + previous_value)
         return self._bars
